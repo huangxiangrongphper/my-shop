b7f9982cfcb9e04fd07af2c638bc72ca204027c1
vagrant <vagrant@homestead>
2018-10-01 17:41:00 +0000

    邮箱验证中间件

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,16 @@ Route::get('/', 'PagesController@root')->name('root');
 /**Laravel 的用户认证路由**/
 Auth::routes();
 
+/**用户邮箱验证路由**/
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
+    // 开始
+    Route::group(['middleware' => 'email_verified'], function() {
+        Route::get('/test', function() {
+            return 'Your email is verified';
+        });
+    });
+    // 结束
+});
+
