5d6839806e0dcb937ea76516505999e64a786bfe
vagrant <vagrant@homestead>
2018-10-05 18:33:52 +0000

    完善假数据填充

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -99,6 +99,10 @@ class CouponCodesController extends Controller
                 return $value ? '是' : '否';
             });
             $grid->created_at('创建时间');
+
+            $grid->actions(function ($actions) {
+                $actions->disableView();
+            });
         });
     }
 
